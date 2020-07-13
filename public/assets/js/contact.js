b7cf614490588454a3a1173285a92bf5c59da602
ashton_morgan <luckyguy73@yahoo.com>
2020-07-12 19:46:22 -0700

    minor changes including color on resume page

--- public/assets/js/contact.js
+++ public/assets/js/contact.js
@@ -1,5 +1,6 @@
 let form = document.querySelector('#form-contact');
 let div = document.querySelector('#form-div');
+
 form.addEventListener('submit', event => {
    event.preventDefault();
    div.innerHTML = `<div id="message-div"><p>Message has been sent!</p></div>`;
